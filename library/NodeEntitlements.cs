f77dc8331cbe803431d489caa5cbfc1026f519ee
Bevan Arps <bevan.arps@microsoft.com>
2017-05-08 14:34:35 +1200

    Simplifying repro

--- library/NodeEntitlements.cs
+++ library/NodeEntitlements.cs
@@ -14,16 +14,6 @@ namespace library
 
         public DateTimeOffset NotAfter { get; } = DateTimeOffset.Now.AddDays(7);
 
-        public ImmutableHashSet<string> Applications { get; }
-
-        public ImmutableHashSet<IPAddress> IpAddresses { get; }
-
         public string Identifier { get; } = "26d3e7ce-548a-44e1-b1d6-07c073e83f72";
-
-        public NodeEntitlements()
-        {
-            Applications = ImmutableHashSet<string>.Empty.Add("app");
-            IpAddresses = ImmutableHashSet<IPAddress>.Empty.Add(IPAddress.Parse("127.0.0.1"));
-        }
     }
 }
--- library/TokenGenerator.cs
+++ library/TokenGenerator.cs
@@ -20,34 +20,32 @@ namespace library
             EncryptingCredentials = encryptingCredentials;
         }
 
-public string Generate(NodeEntitlements entitlements)
-{
-    if (entitlements == null)
-    {
-        throw new ArgumentNullException(nameof(entitlements));
-    }
+        public string Generate(NodeEntitlements entitlements)
+        {
+            if (entitlements == null)
+            {
+                throw new ArgumentNullException(nameof(entitlements));
+            }
 
-    var claims = CreateClaims(entitlements);
-    var claimsIdentity = new ClaimsIdentity(claims);
+            var claims = CreateClaims(entitlements);
+            var claimsIdentity = new ClaimsIdentity(claims);
 
-    var securityTokenDescriptor = new SecurityTokenDescriptor
-    {
-        Subject = claimsIdentity,
-        NotBefore = entitlements.NotBefore.UtcDateTime,
-        Expires = entitlements.NotAfter.UtcDateTime,
-        IssuedAt = DateTimeOffset.Now.UtcDateTime,
-        Issuer = "https://example.com",
-        Audience = "https://example.com",
-        SigningCredentials = SigningCredentials,
-        EncryptingCredentials = EncryptingCredentials
-    };
+            var securityTokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = claimsIdentity,
+                NotBefore = entitlements.NotBefore.UtcDateTime,
+                Expires = entitlements.NotAfter.UtcDateTime,
+                IssuedAt = DateTimeOffset.Now.UtcDateTime,
+                Issuer = "https://example.com",
+                Audience = "https://example.com",
+                SigningCredentials = SigningCredentials,
+                EncryptingCredentials = EncryptingCredentials
+            };
 
-    var handler = new JwtSecurityTokenHandler();
-
-    var token = handler.CreateToken(securityTokenDescriptor);
-
-    return handler.WriteToken(token);
-}
+            var handler = new JwtSecurityTokenHandler();
+            var token = handler.CreateToken(securityTokenDescriptor);
+            return handler.WriteToken(token);
+        }
 
         private List<Claim> CreateClaims(NodeEntitlements entitlements)
         {
@@ -56,17 +54,6 @@ public string Generate(NodeEntitlements entitlements)
                 new Claim("vmid", entitlements.VirtualMachineId),
                 new Claim("id", entitlements.Identifier)
             };
-
-            foreach (var ip in entitlements.IpAddresses)
-            {
-                claims.Add(new Claim("ip", ip.ToString()));
-            }
-
-            foreach (var app in entitlements.Applications)
-            {
-                var claim = new Claim("app", app);
-                claims.Add(claim);
-            }
 
             return claims;
         }
